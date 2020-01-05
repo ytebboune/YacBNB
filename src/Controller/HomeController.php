4210053df78f6161671e09e21964cde6b1878909
ytebboune <tebboune.yacine83@gmail.com>
2020-01-05 18:57:11 +0100

    test package encore

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,12 +12,12 @@ class HomeController extends AbstractController{
     /**
      * @Route("/", name="homepage")
      */
-    public function home(){
+    public function home(AdRepository $adRepo, UserRepository $userRepo){
         return $this->render(
             'home.html.twig',
             [
-                'title' => 'Bonjour à tous',
-                'age' => 16
+                'ads' => $adRepo -> findBestAds(3),
+                'users' => $userRepo->findBestUsers(2)
             ]
         );
     }
